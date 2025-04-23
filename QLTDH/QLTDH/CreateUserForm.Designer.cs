dc5f1ffb0415fb838df8c3de314ffd82be9222b0
vunewbie <vulocninh1@gmail.com>
2025-04-23 09:59:35 +0700

    Update Create User UI

--- QLTDH/QLTDH/CreateUserForm.Designer.cs
+++ QLTDH/QLTDH/CreateUserForm.Designer.cs
@@ -33,7 +33,6 @@
             this.txbUsername = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
             this.lblUsername = new System.Windows.Forms.Label();
-            this.lblNewUser = new System.Windows.Forms.Label();
             this.pbPassword = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbPassword)).BeginInit();
             this.SuspendLayout();
@@ -41,11 +40,11 @@
             // btnCreateUser
             // 
             this.btnCreateUser.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.btnCreateUser.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnCreateUser.Location = new System.Drawing.Point(280, 339);
-            this.btnCreateUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreateUser.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCreateUser.Location = new System.Drawing.Point(162, 238);
+            this.btnCreateUser.Margin = new System.Windows.Forms.Padding(4);
             this.btnCreateUser.Name = "btnCreateUser";
-            this.btnCreateUser.Size = new System.Drawing.Size(276, 45);
+            this.btnCreateUser.Size = new System.Drawing.Size(144, 39);
             this.btnCreateUser.TabIndex = 11;
             this.btnCreateUser.Text = "Tạo tài khoản";
             this.btnCreateUser.UseVisualStyleBackColor = false;
@@ -54,26 +53,26 @@
             // txbPassword
             // 
             this.txbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbPassword.Location = new System.Drawing.Point(280, 239);
-            this.txbPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbPassword.Location = new System.Drawing.Point(194, 165);
+            this.txbPassword.Margin = new System.Windows.Forms.Padding(4);
             this.txbPassword.Name = "txbPassword";
-            this.txbPassword.Size = new System.Drawing.Size(276, 30);
+            this.txbPassword.Size = new System.Drawing.Size(214, 30);
             this.txbPassword.TabIndex = 10;
             // 
             // txbUsername
             // 
             this.txbUsername.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbUsername.Location = new System.Drawing.Point(280, 158);
-            this.txbUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbUsername.Location = new System.Drawing.Point(194, 76);
+            this.txbUsername.Margin = new System.Windows.Forms.Padding(4);
             this.txbUsername.Name = "txbUsername";
-            this.txbUsername.Size = new System.Drawing.Size(276, 30);
+            this.txbUsername.Size = new System.Drawing.Size(214, 30);
             this.txbUsername.TabIndex = 9;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lblPassword.Location = new System.Drawing.Point(75, 241);
+            this.lblPassword.Location = new System.Drawing.Point(22, 170);
             this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(91, 23);
@@ -84,33 +83,20 @@
             // 
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lblUsername.Location = new System.Drawing.Point(75, 160);
+            this.lblUsername.Location = new System.Drawing.Point(22, 78);
             this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(132, 23);
             this.lblUsername.TabIndex = 7;
             this.lblUsername.Text = "Tên đăng nhập";
             // 
-            // lblNewUser
-            // 
-            this.lblNewUser.AutoSize = true;
-            this.lblNewUser.BackColor = System.Drawing.SystemColors.Control;
-            this.lblNewUser.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lblNewUser.Location = new System.Drawing.Point(297, 59);
-            this.lblNewUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblNewUser.Name = "lblNewUser";
-            this.lblNewUser.Size = new System.Drawing.Size(282, 32);
-            this.lblNewUser.TabIndex = 6;
-            this.lblNewUser.Text = "Thêm người dùng mới";
-            this.lblNewUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // pbPassword
             // 
             this.pbPassword.Image = global::QLTDH.Properties.Resources.eye_off;
-            this.pbPassword.Location = new System.Drawing.Point(528, 241);
+            this.pbPassword.Location = new System.Drawing.Point(380, 167);
             this.pbPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbPassword.Name = "pbPassword";
-            this.pbPassword.Size = new System.Drawing.Size(28, 23);
+            this.pbPassword.Size = new System.Drawing.Size(26, 23);
             this.pbPassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbPassword.TabIndex = 20;
             this.pbPassword.TabStop = false;
@@ -120,16 +106,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 503);
+            this.ClientSize = new System.Drawing.Size(484, 311);
             this.Controls.Add(this.pbPassword);
             this.Controls.Add(this.btnCreateUser);
             this.Controls.Add(this.txbPassword);
             this.Controls.Add(this.txbUsername);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.lblNewUser);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CreateUserForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thêm người dùng";
@@ -146,7 +131,6 @@
         private System.Windows.Forms.TextBox txbUsername;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.Label lblNewUser;
         private System.Windows.Forms.PictureBox pbPassword;
     }
 }
