efb170fbf710b827af66898b7bd47b1b1c2dd10b
nhyy204 <22120425@student.hcmus.edu.vn>
2025-05-12 23:05:18 +0700

    Done auditing 3a, 3b

--- Init_Scripts/11_audit.sql
+++ Init_Scripts/11_audit.sql
@@ -75,3 +75,132 @@ EXCEPTION
     RAISE_APPLICATION_ERROR(-20002, 'Lỗi khi tắt audit: ' || SQLERRM);
 END;
 /
+
+--==============================================================================
+--Tạo chính sách FGA
+--==============================================================================
+--Hành vi cập nhật quan hệ ĐANGKY tại các trường liên quan đến điểm số nhưng
+--người đó không thuộc vai trò “NV PKT”.
+BEGIN
+  FOR rec IN (SELECT policy_name 
+              FROM DBA_AUDIT_POLICIES 
+              WHERE object_schema = 'QLTDH' 
+              AND object_name = 'DANGKY' 
+              AND policy_name = 'UPDATE_SCORE') 
+  LOOP
+    DBMS_FGA.DROP_POLICY(
+      object_schema => 'QLTDH',    
+      object_name   => 'DANGKY',   
+      policy_name   => 'UPDATE_SCORE'  
+    );
+  END LOOP;
+END;
+/
+BEGIN
+  DBMS_FGA.ADD_POLICY(
+    object_schema   => 'QLTDH',             
+    object_name     => 'DANGKY',              
+    policy_name     => 'UPDATE_SCORE',     
+    audit_condition => 'VAITRO != ''NV PKT''',
+    audit_column    => 'DIEMTH,DIEMQT,DIEMCK,DIEMTK',  
+    statement_types => 'UPDATE'                
+  );
+END;
+/
+
+--Câu lệnh kiểm tra
+--UPDATE QLTDH.DANGKY
+--SET DIEMTH = 7.0
+--WHERE MASV = 'SV0001' AND MAMM='MM0002';
+--
+--CONNECT NVPKT0001/NVPKT0001@localhost:1521/QUANLYTRUONGDAIHOC;
+--UPDATE QLTDH.DANGKY
+--SET DIEMTH = 6.0
+--WHERE MASV = 'SV0001' AND MAMM='MM0002';
+
+--Kiểm tra audit
+--CONNECT QLTDH/admin123@localhost:1521/QUANLYTRUONGDAIHOC;
+--SELECT * FROM DBA_FGA_AUDIT_TRAIL
+--WHERE object_name = 'DANGKY';
+
+
+--==============================================================================
+--Hành vi của người dùng (không thuộc vai trò “NV TCHC”) có thể đọc trên
+--trường LUONG, PHUCAP của người khác ở quan hệ NHANVIEN.
+BEGIN
+  FOR rec IN (SELECT policy_name 
+              FROM DBA_AUDIT_POLICIES 
+              WHERE object_schema = 'QLTDH' 
+              AND object_name = 'NHANVIEN' 
+              AND policy_name = 'SELECT_EMPLOYEE') 
+  LOOP
+    DBMS_FGA.DROP_POLICY(
+      object_schema => 'QLTDH',    
+      object_name   => 'NHANVIEN',   
+      policy_name   => 'SELECT_EMPLOYEE'  
+    );
+  END LOOP;
+END;
+/
+BEGIN
+  DBMS_FGA.ADD_POLICY(
+    object_schema   => 'QLTDH',             
+    object_name     => 'NHANVIEN',              
+    policy_name     => 'SELECT_EMPLOYEE',     
+    audit_condition => 'VAITRO != ''NV TCHC'' AND MANV != SYS_CONTEXT(''USERENV'', ''SESSION_USER'')',
+    audit_column    => 'LUONG,PHUCAP',  
+    statement_types => 'SELECT'                
+  );
+END;
+/
+
+--Câu lệnh kiểm tra
+--GRANT SELECT ON QLTDH.NHANVIEN TO "NVPKT0001";
+--Đăng nhập bằng acc 'NVPKT0001'
+--SELECT LUONG, PHUCAP FROM QLTDH.NHANVIEN WHERE MANV='NVPKT0001'; --không bị audit
+--SELECT LUONG, PHUCAP FROM QLTDH.NHANVIEN WHERE MANV='NVPKT0002'; --bị audit
+
+--Kiểm tra audit
+--SELECT * FROM DBA_FGA_AUDIT_TRAIL
+--WHERE object_name = 'NHANVIEN';
+
+--==============================================================================
+--Hành vi của người dùng (không thuộc vai trò “NV TCHC”) cập nhật ở 
+--quan hệ NHANVIEN.
+BEGIN
+  FOR rec IN (SELECT policy_name 
+              FROM DBA_AUDIT_POLICIES 
+              WHERE object_schema = 'QLTDH' 
+              AND object_name = 'NHANVIEN' 
+              AND policy_name = 'UPDATE_EMPLOYEE') 
+  LOOP
+    DBMS_FGA.DROP_POLICY(
+      object_schema => 'QLTDH',    
+      object_name   => 'NHANVIEN',   
+      policy_name   => 'UPDATE_EMPLOYEE'  
+    );
+  END LOOP;
+END;
+/
+BEGIN
+  DBMS_FGA.ADD_POLICY(
+    object_schema   => 'QLTDH',             
+    object_name     => 'NHANVIEN',              
+    policy_name     => 'UPDATE_EMPLOYEE',     
+    audit_condition => 'VAITRO != ''NV TCHC''',
+    audit_column    => 'HOTEN,PHAI,NGSINH,LUONG,PHUCAP,DT,VAITRO,MADV',  
+    statement_types => 'UPDATE'                
+  );
+END;
+/
+
+--Câu lệnh kiểm tra
+--GRANT UPDATE ON QLTDH.NHANVIEN TO "NVPKT0001";
+--Đăng nhập bằng acc 'NVPKT0001'
+--UPDATE QLTDH.NHANVIEN SET PHAI='Nam' WHERE MANV='NVPKT0002';
+--UPDATE QLTDH.NHANVIEN SET PHAI='Nam' WHERE MANV='NVPKT0001';
+
+--Kiểm tra audit
+--SELECT * FROM DBA_FGA_AUDIT_TRAIL
+--WHERE object_name = 'NHANVIEN';
+
